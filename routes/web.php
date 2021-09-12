7480736285cd91560cf53db08ce1be48201b456a
yasu-swift <sp4c72q9@icloud.com>
2021-09-12 23:46:58 +0900

    計算アプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+Route::get('/route/hello', function () {
+return '<h1>計算アプリ</h1>';
+});
+
+Route::get('/calcs/{a}/{b}/{c}', function ($a, $b, $c) {
+    return view('message.con', [
+        'a' => $a,
+        'b' => $b,
+        'c' => $c,
+    ]);
+});
+
+
+
+Route::get('calcs/{a}/{b}/{c}', [App\Http\Controllers\CalcController::class, 'con']);
+
