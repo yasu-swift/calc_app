4414b67d84ff36ad0cfbf420f0c8f2a02d26da62
yasu-swift <sp4c72q9@icloud.com>
2021-09-14 23:14:17 +0900

    課題修正

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\Echo_;
-
 class CalcController extends Controller
 {
     public function con($a, $b, $c,)
 {
     if ($b == 'addition') {
         $answer = $a + $c;
-    }elseif ($b =='subtraction'){
+    } elseif ($b =='subtraction'){
         $answer = $a - $c;
-    }elseif ($b == 'multiplication'){
+    } elseif ($b == 'multiplication'){
         $answer = $a * $c;
-    }elseif ($b == 'division'){
+    } elseif ($b == 'division'){
         $answer = $a / $c;
-    }else{
+    } else {
         $answer = '計算できません';
-        // echo '計算できません';
     }
 
     return view('message.con', [
